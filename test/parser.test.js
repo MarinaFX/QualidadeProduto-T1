318be1321b29acabe499569feb84adff6a559e85
Marina Moreira <38846018+MarinaFX@users.noreply.github.com>
2022-09-03 20:28:03 -0300

    test: add missing test cases

--- test/parser.test.js
+++ test/parser.test.js
@@ -1,5 +1,6 @@
 const parserAge2Days = require('../src/index');
 
+//Case test 400
 describe('Parser parser 2 days', () => {
     test('when days are 400', () => {
         const days = 400;
@@ -8,3 +9,23 @@ describe('Parser parser 2 days', () => {
         expect(parserAge2Days(days)).toEqual(expectedAnswer);
     });
 }) 
+
+//Case test 800
+describe('Parser parser 2 days', () => {
+    test('when days are 800', () => {
+        const days = 800;
+        const expectedAnswer = `\n 2 ano(s) \n 2 mes(es) \n 10 dia(s)`
+
+        expect(parserAge2Days(days)).toEqual(expectedAnswer);
+    });
+})
+
+//Case test 30
+describe('Parser parser 2 days', () => {
+    test('when days are 30', () => {
+        const days = 30;
+        const expectedAnswer = `\n 0 ano(s) \n 1 mes(es) \n 0 dia(s)`
+
+        expect(parserAge2Days(days)).toEqual(expectedAnswer);
+    });
+})
