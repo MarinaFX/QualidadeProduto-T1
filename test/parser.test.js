b893bdc8147465f60ec1746f10cd161c37215d83
Felipe Pritsch Fahrion <felipefahrion@MacBook-Pro-de-Felipe-2.local>
2022-09-05 18:13:17 -0300

    refactor unit test

--- test/parser.test.js
+++ test/parser.test.js
@@ -1,31 +1,25 @@
 const parserAge2Days = require('../src/index');
 
 //Case test 400
-describe('Parser parser 2 days', () => {
+describe('Parser age 2 days', () => {
     test('when days are 400', () => {
         const days = 400;
         const expectedAnswer = `\n 1 ano(s) \n 1 mes(es) \n 5 dia(s)`
 
         expect(parserAge2Days(days)).toEqual(expectedAnswer);
     });
-}) 
 
-//Case test 800
-describe('Parser parser 2 days', () => {
     test('when days are 800', () => {
         const days = 800;
         const expectedAnswer = `\n 2 ano(s) \n 2 mes(es) \n 10 dia(s)`
 
         expect(parserAge2Days(days)).toEqual(expectedAnswer);
     });
-})
 
-//Case test 30
-describe('Parser parser 2 days', () => {
     test('when days are 30', () => {
         const days = 30;
         const expectedAnswer = `\n 0 ano(s) \n 1 mes(es) \n 0 dia(s)`
 
         expect(parserAge2Days(days)).toEqual(expectedAnswer);
     });
-})
+}) 
